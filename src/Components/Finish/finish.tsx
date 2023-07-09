f9306394f8e2a618acf064f4b16a08b379d489ff
msilis <mikssilis@gmail.com>
2023-07-09 22:11:15 +0100

    new story redirect

--- src/Components/Finish/finish.tsx
+++ src/Components/Finish/finish.tsx
@@ -4,22 +4,33 @@ import { useEffect } from "react";
 import { storyEdit } from "../Utilities/storyEdit";
 import { StoryEdit } from "../Interfaces/editProps";
 import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 export default function Finish(
   props: FinishState & StoryEdit
 ): React.ReactNode {
   const displayStory = storyEdit({ storyState: props.storyState });
+  const navigate = useNavigate();
 
   useEffect(() => {
     storyEdit({ storyState: props.storyState });
+    //Disable empty array eslint complaint -- I really do want it to run only once!
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  //New story click functionality
+  const newClick = () => {
+    navigate("/");
+  };
 
   return (
     <div className={style.storyContainer}>
       <h1>Here is your story:</h1>
       <pre className={style.storyStyle}>{displayStory}</pre>
       <div className={style.newStory}>
-        <Button className={style.button}>New Story</Button>
+        <Button className={style.button} onClick={newClick}>
+          New Story
+        </Button>
       </div>
     </div>
   );
