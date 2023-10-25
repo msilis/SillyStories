3ca5f114e3e09242ee2870bc8d3ece5fc40d245d
msilis <mikssilis@gmail.com>
2023-10-25 12:06:54 +0100

    city and food tests

--- src/Components/City/city.test.tsx
+++ src/Components/City/city.test.tsx
@@ -1,16 +1,16 @@
 import { expect, it, describe } from 'vitest';
 import { render } from '@testing-library/react';
-import Food from './food';
+import City from './city';
 import { BrowserRouter } from 'react-router-dom';
 
 describe('Food component', () => {
     it('should render the component', () => {
         const { getByText } = render(
             <BrowserRouter>
-                <Food />
+                <City />
             </BrowserRouter>
         );
-        expect(getByText('Write the name of a food:')).toBeTruthy();
+        expect(getByText('Give me the name of a city!')).toBeTruthy();
     });
 });
 
@@ -18,7 +18,7 @@ describe('Food component', () => {
     it('should render the component', () => {
         const { getByText } = render(
             <BrowserRouter>
-                <Food />
+                <City />
             </BrowserRouter>
         );
         expect(getByText('Next')).toBeTruthy();
@@ -29,7 +29,7 @@ describe('FriendName component', () => {
     it('should render the component', () => {
         const { getByText } = render(
             <BrowserRouter>
-                <Food />
+                <City />
             </BrowserRouter>
         );
         expect(getByText('Back')).toBeTruthy();
