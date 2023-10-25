af2f772b26a2000209d0cf118ea551c9f1f76f3c
msilis <mikssilis@gmail.com>
2023-10-25 17:03:02 +0100

    linting, formatting and remove magic string from city test

--- src/Components/Animal/animal.test.tsx
+++ src/Components/Animal/animal.test.tsx
@@ -3,12 +3,19 @@ import { render } from '@testing-library/react';
 import Animal from './animal';
 import { BrowserRouter } from 'react-router-dom';
 import { CARD_TEXT } from '../../ui-text/ui-text';
+import { type SetStateAction } from 'react';
 
 describe('Food component', () => {
     it('should render the component', () => {
         const { getByText } = render(
             <BrowserRouter>
-                <Animal />
+                <Animal
+                    setStoryState={function (
+                        _value: SetStateAction<string>
+                    ): void {
+                        throw new Error('Function not implemented.');
+                    }}
+                />
             </BrowserRouter>
         );
         expect(getByText(CARD_TEXT.animalCardTitle)).toBeTruthy();
@@ -19,7 +26,13 @@ describe('Food component', () => {
     it('should render the component', () => {
         const { getByText } = render(
             <BrowserRouter>
-                <Animal />
+                <Animal
+                    setStoryState={function (
+                        _value: SetStateAction<string>
+                    ): void {
+                        throw new Error('Function not implemented.');
+                    }}
+                />
             </BrowserRouter>
         );
         expect(getByText('Finish')).toBeTruthy();
@@ -30,7 +43,13 @@ describe('FriendName component', () => {
     it('should render the component', () => {
         const { getByText } = render(
             <BrowserRouter>
-                <Animal />
+                <Animal
+                    setStoryState={function (
+                        _value: SetStateAction<string>
+                    ): void {
+                        throw new Error('Function not implemented.');
+                    }}
+                />
             </BrowserRouter>
         );
         expect(getByText('Back')).toBeTruthy();
--- src/Components/City/city.test.tsx
+++ src/Components/City/city.test.tsx
@@ -2,6 +2,7 @@ import { expect, it, describe } from 'vitest';
 import { render } from '@testing-library/react';
 import City from './city';
 import { BrowserRouter } from 'react-router-dom';
+import { CARD_TEXT } from '../../ui-text/ui-text';
 
 describe('Food component', () => {
     it('should render the component', () => {
@@ -10,7 +11,7 @@ describe('Food component', () => {
                 <City />
             </BrowserRouter>
         );
-        expect(getByText('Give me the name of a city!')).toBeTruthy();
+        expect(getByText(CARD_TEXT.cityCardTitle)).toBeTruthy();
     });
 });
 
